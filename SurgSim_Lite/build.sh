0f970e71cb6adbf47f34f2aacd9fdef76acbb871
Rasmus Anthin <scratty@gmail.com>
2024-06-01 17:51:27 +0200

    Adding local / relative header search path compiler flag here since I removed it from the base script in Core.

--- SurgSim_Lite/build.sh
+++ SurgSim_Lite/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-../../lib/Core/build.sh surgsim_lite $1
+os_name=$(uname)
+
+if [[ $os_name == *"Darwin"* ]]; then
+  additional_flags="-I../../lib"
+else
+  additional_flags="-I../../lib"
+  # export BUILD_PKG_CONFIG_MODULES='openal'
+fi
+
+../../lib/Core/build.sh pilot_episode "$1" "${additional_flags[@]}"
