b523f317697e5c9c2e7c9a3f630591f42d0301ed
Rasmus Anthin <scratty@gmail.com>
2024-09-11 17:04:24 +0200

    build.sh: * Capturing and re-returning failure exit code from base build.sh script on Core.git.

--- SurgSim_Lite/build.sh
+++ SurgSim_Lite/build.sh
@@ -11,6 +11,14 @@ fi
 
 ../../lib/Core/build.sh surgsim_lite "$1" "${additional_flags[@]}"
 
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
+
 ### Post-Build Actions ###
 
 cp title.txt bin/
